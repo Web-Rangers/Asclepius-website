21eed7e061cdd772160a36d11be9a09ec0bb29fb
Cube00 <coller.vato@gmail.com>
2022-11-21 16:11:35 +0400

    checkout

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ import { Carousel } from 'react-responsive-carousel';
 import MainSlider from '../components/contents/mainSlider';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, message, Space } from 'antd';
+import * as ANT from 'antd';
+import 'antd/dist/antd.css';
 
 const onClick = ({ key }) => {
 	message.info(`Click on item ${key}`);
@@ -35,7 +37,7 @@ const items = [
 	},
 ];
 
-function Home({ clinics, doctors, frelancers }) {
+function Home({ clinics, doctors, frelancers, categories }) {
 	const [clinicData, setClinicData] = useState([]);
 	const [doctorsData, setDoctorsData] = useState([]);
 	const [imgData, setImgData] = useState([]);
@@ -103,34 +105,79 @@ function Home({ clinics, doctors, frelancers }) {
 		}
 	}, [doctors, windowSize.width]);
 
+	const items = [
+		{
+		  key: '1',
+		  label: (
+			<a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+			  1st menu item
+			</a>
+		  ),
+		},
+		{
+		  key: '2',
+		  label: (
+			<a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+			  2nd menu item
+			</a>
+		  ),
+		},
+		{
+		  key: '3',
+		  label: (
+			<a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+			  3rd menu item
+			</a>
+		  ),
+		},
+	  ];
+
 	return (
 		<div className={classes.homePageContainer}>
 			<div className={classes.catalogContainer}>
-				{catalogData.map((item, index) => (
-					<span
-						key={index}
-						className={classes.catalogTextStyle}
-					>
-						{item.name}
-					</span>
-				))}
+				{categories?.map((item, index) => {
+					const subCategories = categories.filter((e)=> e.parentCategoryId == item.id);
+					const items = subCategories.map((e, key)=> {
+						return {
+							key: key,
+							label: (
+							  <a target="_blank" rel="noopener noreferrer" href={`/${e.title}`}>
+								{e.title}
+							  </a>
+							),
+						}
+					})
+
+					return <>
+					{
+						item.parentCategoryId === null && 
+						(
+							items.length > 0 ?
+							<Dropdown
+								menu={{
+									items,
+								}}
+								placement="bottom"
+								overlayClassName={classes.dropdown}
+							>
+								<span
+									key={index}
+									className={classes.catalogTextStyle}
+								>
+									{item.title}
+								</span>
+							</Dropdown> : 
+							<span
+								key={index}
+								className={classes.catalogTextStyle}
+							>
+								{item.title}
+							</span>
+						)
+					}
+					</>
+				})}
 			</div>
-			{/* <div className={classes.dropDownContainer}>
-				<span>hello</span>
-			</div> */}
-			<Dropdown
-				menu={{
-					items,
-					onClick,
-				}}
-			>
-				<a onClick={(e) => e.preventDefault()}>
-					<Space>
-						Hover me, Click menu item
-						<DownOutlined />
-					</Space>
-				</a>
-			</Dropdown>
 			<div>
 				<div className={classes.firstPart}>
 					<div className={classes.showSlider}>
@@ -241,6 +288,9 @@ export const getServerSideProps = async () => {
 	const getFreelancerDoc = await getData(
 		`${API_URL}/asclepius/v1/api/doctors/freelancers?page=0&size=5`
 	);
+	const getCategories = await getData(
+		`${API_URL}/asclepius/v1/api/categories`
+	);
 
 	return {
 		props: {
@@ -252,6 +302,7 @@ export const getServerSideProps = async () => {
 					  }),
 			doctors: getDoctors?.length === 0 ? null : getDoctors,
 			frelancers: getFreelancerDoc?.length === 0 ? null : getFreelancerDoc,
+			categories: getCategories
 		},
 	};
 };
