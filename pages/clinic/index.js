7f7d3ada6f34f9c6f38952242dd40e05afdad914
Irina Endeladze <ir.endeladze@gmail.com>
2022-12-05 18:25:54 +0400

    fixed clinic page

--- pages/clinic/index.js
+++ pages/clinic/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import styles from '../../styles/pages/clinic.module.css';
 import classNames from 'classnames';
 import CheckBox from '../../components/ui/CheckBox';
@@ -8,73 +8,102 @@ import Link from 'next/link';
 import { getData } from '../../components/request';
 import Image from 'next/image';
 
-export default function Clinic({clinics}) {
-    console.log(clinics, 'clinics')
-    const [blockId, setBlockId] = useState('');
-    const [done, setDone] = useState(false);
-    
-    return <>
-        <div className={styles.clinicBody}>
-            <div className={styles.clinicContent}>
-                <div className={styles.clinicLogoContent}>
-                    <img src="/clinicDocLogo.svg" className={styles.logo} alt="" />
-                </div>
-                <div className={styles.clinicAnalyisis}>
-                    <div className={styles.analysH}>
-                        <h2>Choose Clinic</h2>
-                    </div>
-                    <div className={styles.searchBlock}>
-                        {
-                            blockId == '' && 
-                            <input 
-                                className={styles.searchInput} 
-                                type="search" 
-                                placeholder="search" 
-                            />
-                        }
-                        <div className={classNames(styles.clinicsList, {
-                            [styles.activeSearch]: true,
-                            [styles.activeClinicBlocks]: blockId !== '' && true
-                        })}>
-                            {
-                                blockId == '' && 
-                                <div className={styles.listH}>
-                                    <h2>Clinics</h2>
-                                </div>
-                            }
-                           
-                           
-                            {clinics?.content && 
-                                clinics?.content?.map((item, index) => (
-                                    <Link href={`clinic/branches/${item?.id}`} key={index}>
-                                        <div className={styles.listItem}>
-                                            <div
-                                                className={classNames(styles.clinic, {
-                                                    [styles.activeAnBlock]: blockId !== '' && true
-                                                })}
-                                            >
-                                                <Image src={item?.logoUrl} width={40} height={40} alt="" />
-                                                <h2>{item?.displayName}</h2>
-                                                <img className={styles.arrow} src="/clinArrow.svg" alt="" />
-                                            </div>
-                                        </div>
-                                    </Link>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </>
+export default function Clinic({ clinics }) {
+	console.log(clinics, 'clinics');
+	const [blockId, setBlockId] = useState('');
+	const [done, setDone] = useState(false);
+
+	return (
+		<>
+			<div className={styles.clinicBody}>
+				<div className={styles.clinicContent}>
+					<div className={styles.clinicLogoContent}>
+						<img
+							src='/clinicDocLogo.svg'
+							className={styles.logo}
+							alt=''
+						/>
+					</div>
+
+					<div className={styles.clinicAnalyisis}>
+						<div className={styles.contentHeader}>
+							<div className={styles.backContainer}>
+								<img
+									src='/arrowLeftSlider.svg'
+									alt=''
+								/>
+								<span>Back to profile</span>
+							</div>
+							<div className={styles.analysH}>
+								<h2>Choose Clinic</h2>
+							</div>
+						</div>
+						<div className={styles.searchBlock}>
+							{blockId == '' && (
+								<input
+									className={styles.searchInput}
+									type='search'
+									placeholder='search'
+								/>
+							)}
+							<div
+								className={classNames(styles.clinicsList, {
+									[styles.activeSearch]: true,
+									[styles.activeClinicBlocks]: blockId !== '' && true,
+								})}
+							>
+								{blockId == '' && (
+									<div className={styles.listH}>
+										<h2>Clinics</h2>
+									</div>
+								)}
+								<div className={styles.list}>
+									{clinics?.content &&
+										clinics?.content?.map((item, index) => (
+											<Link
+												href={`clinic/branches/${item?.id}`}
+												key={index}
+											>
+												<div className={styles.listItem}>
+													<div
+														className={classNames(styles.clinic, {
+															[styles.activeAnBlock]: blockId !== '' && true,
+														})}
+													>
+														<Image
+															src={item?.logoUrl}
+															width={40}
+															height={40}
+															alt=''
+														/>
+														<h2>{item?.displayName}</h2>
+														<img
+															className={styles.arrow}
+															src='/clinArrow.svg'
+															alt=''
+														/>
+													</div>
+												</div>
+											</Link>
+										))}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export const getStaticProps = async () => {
-    const getClinics = await getData('https://asclepius.pirveli.ge/asclepius/v1/api/clinics/?page=0&size=10')
+	const getClinics = await getData(
+		'https://asclepius.pirveli.ge/asclepius/v1/api/clinics/?page=0&size=10'
+	);
 
-    return {
-        props: {
-            clinics: getClinics
-        },
-    }
-}
+	return {
+		props: {
+			clinics: getClinics,
+		},
+	};
+};
